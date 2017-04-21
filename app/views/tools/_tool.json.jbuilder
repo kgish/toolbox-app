cb507ed7b79777f3263ea8647a12cad7dc363cbb
Kiffin Gish <kiffin.gish@planet.nl>
2017-04-21 23:19:33 +0200

    Devise users and resource tools.

---
+++ app/views/tools/_tool.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! tool, :id, :icon, :color, :title, :text, :created_at, :updated_at
+json.url tool_url(tool, format: :json)
---
+++ db/migrate/20170421211629_create_tools.rb
@@ -0,0 +1,12 @@
+class CreateTools < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tools do |t|
+      t.string :icon
+      t.string :color
+      t.string :title
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
