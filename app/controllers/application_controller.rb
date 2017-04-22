d1a4698b304cbaeae4d50b7d80607b6dcb2a5ce3
Kiffin Gish <kiffin.gish@planet.nl>
2017-04-22 12:49:50 +0200

    Login with admin and default tools displayed on main page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # Force user to redirect to login page if the user was not logged in
+  before_action :authenticate_user!
 end
