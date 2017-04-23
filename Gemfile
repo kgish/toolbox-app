608c8bc1df6c8e37bbfcfb12e0ce3c2eee5a8167
Kiffin Gish <kiffin.gish@planet.nl>
2017-04-23 18:17:22 +0200

    rail db:seed to create admin user

--- Gemfile
+++ Gemfile
@@ -52,5 +52,5 @@ group :development do
     gem 'capistrano-rails',   require: false
     gem 'capistrano-bundler', require: false
     gem 'capistrano3-puma',   require: false
-    gem 'capistrano-rails-console', require: false
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  username: 'kiffin',
+  firstname: 'Kiffin',
+  lastname: 'Gish',
+  email: 'kiffin.gish@planet.nl',
+  password: 'adm1n-t00lb0x',
+  admin: true
+)
