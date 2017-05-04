32759ef538fc9f7ee4ea389d3f4f70f39fb89b4a
Kiffin Gish <kiffin.gish@planet.nl>
2017-05-04 22:37:55 +0200

    Fixed #5. Logged in user should have a profile page.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   get '/about' => 'about#index'
   get '/contact' => 'contact#index'
+  get '/profile' => 'profile#show'
 
   mount Bootsy::Engine => '/bootsy', as: 'bootsy'
 
