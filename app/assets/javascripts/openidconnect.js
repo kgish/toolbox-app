65649b3bf79104726171b6324edb60724fca731f
Kiffin Gish <kiffin.gish@planet.nl>
2017-05-31 13:53:23 +0200

    Login-callback page for openid.

--- app/assets/javascripts/openidconnect.js
+++ app/assets/javascripts/openidconnect.js
@@ -2,7 +2,7 @@
 
 var clientInfo = {
     // client_id : '(your-client-id)',
-    redirect_uri : 'https://demo.participation.tools/openidcallback'
+    redirect_uri : 'https://demo.participation.tools/login-callback.html'
 };
 
 var providerInfo = OIDC.discover('https://sso.participation.tools');
