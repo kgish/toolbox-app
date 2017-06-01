f6cdae8bfc3be47855f72c409df17b34abd1efbd
Kiffin Gish <kiffin.gish@planet.nl>
2017-06-01 22:51:11 +0200

    userinfo_params

--- app/controllers/openiduserinfo_controller.rb
+++ app/controllers/openiduserinfo_controller.rb
@@ -1,10 +1,17 @@
 class OpeniduserinfoController < ApplicationController
+  before_action :set_userinfo
   skip_before_action :authenticate_user!
 
   def userinfo
 
-    render json: params, status: :ok
+    render json: userinfo_params, status: :ok
 
   end
+
+    private
+
+    def userinfo_params
+      params.require(:userinfo).permit(:updated_at, :uid, :name, :given_name, :family_name, :email, :sub, :role, :o)
+    end
 
 end
