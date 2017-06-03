87cd24142b2cc5745150d59341667f0c9cdc600f
Kiffin Gish <kiffin.gish@planet.nl>
2017-06-03 21:45:45 +0200

    Force devise sign_in if user found openid.

--- app/assets/javascripts/openidconnect.js
+++ app/assets/javascripts/openidconnect.js
@@ -4,27 +4,22 @@ $(function() {
             client_id: '@!FDE5.5990.3FE6.E7A6!0001!8BC6.A3A2!0008!0EBC.3A8D.EBD4.2C37',
             client_secret: 'b63221fe-d417-4956-a6c5-3698b30be2bf',
             redirect_uri: 'https://demo.participation.tools/openidcallback'
+        },
+        scope_and_response_type = {
+            scope: 'openid profile email clientinfo',
+            response_type: 'token id_token'
         };
 
-        //console.log('providerInfo');
         var providerInfo = OIDC.discover('https://sso.participation.tools');
-        //console.log(JSON.stringify(providerInfo));
-        //console.log('setClientInfo');
         OIDC.setClientInfo(clientInfo);
-        //console.log('setProviderInfo');
         OIDC.setProviderInfo(providerInfo);
-        //console.log('storeInfo');
         OIDC.storeInfo(providerInfo, clientInfo);
+
         // Remove State and Nonce from previous session
         sessionStorage.removeItem('state');
         sessionStorage.removeItem('nonce');
-        //console.log('sessionStorage=' + JSON.stringify(sessionStorage));
-        //console.log('loginRequest');
-        loginRequest = OIDC.generateLoginRequest({
-            scope: 'openid profile email',
-            response_type: 'token id_token'
-        });
-        //console.log(JSON.stringify(loginRequest));
+
+        loginRequest = OIDC.generateLoginRequest(scope_and_response_type);
 
         // Enable word wrap for long url.
         loginRequest.url = loginRequest.url.replace(/([?&])/g, '<br/>$1');
@@ -33,7 +28,7 @@ $(function() {
         $('#loginRequest').html(JSONObjToHTMLTable(loginRequest));
 
         $('#authenticate').click(function () {
-            OIDC.login({scope: 'openid profile email', response_type: 'token id_token'});
+            OIDC.login(scope_and_response_type);
         });
 
         var toggle = $('#toggle');
